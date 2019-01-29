ebbad1b6e8761087efea6fe155178a07b60bf156
Unkeptroadrash <mmojunior@hotmail.com>
2019-01-29 11:40:07 -0800

    an example of lists

---
+++ scripts/lists.py
@@ -0,0 +1,18 @@
+numbers = []
+strings = []
+names = ["John", "William", "Pythagoras"]
+
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+strings.append("Hello")
+strings.append("World")
+
+last_name = names[2]
+
+print(numbers)
+print(strings)
+print(names)
+
+print("Last name in the list names: ", names[2])
