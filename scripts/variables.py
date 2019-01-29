a23fb98c1288b3bf0787a0a9e9c66239ed091618
Unkeptroadrash <mmojunior@hotmail.com>
2019-01-29 11:32:49 -0800

    variables

---
+++ scripts/variables.py
@@ -0,0 +1,10 @@
+a, b = 7.0, 1
+mystring = "This is a string"
+
+print("Variable a is ", type(a))
+print("Variable b is ", type(b))
+print("Variable mystring is ", type(mystring))
+
+print("The value of a is ", a)
+print("The value of b is ", b)
+print("The value of mystring is ", mystring)
