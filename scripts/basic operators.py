811321275cc2da379e3f17daeab2b5990c616c12
Unkeptroadrash <mmojunior@hotmail.com>
2019-01-29 11:51:39 -0800

    lists with basic operators

---
+++ scripts/basic operators.py
@@ -0,0 +1,15 @@
+x = [1,2,3]
+y = [4,5,6]
+
+x_and_y = x + y
+
+mystring1 = ["Hello", "world", "!"]
+mystring2 = mystring1 * 10
+
+print(x)
+print(y)
+print(x_and_y)
+print(mystring1)
+print(mystring2)
+
+print("the list mystring2 is huge with ", len(mystring2), "Objects!")
